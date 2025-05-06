ceb2e021512b876c91be5fb966ca3ed626a59df9
Hamzah <hamzahnafalah@gmail.com>
2025-05-06 14:26:00 +0700

    update modules

--- src/Contracts/Disease.php
+++ src/Contracts/Disease.php
@@ -5,7 +5,7 @@ namespace Hanafalah\ModuleDisease\Contracts;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Pagination\LengthAwarePaginator;
-use Hanafalah\LaravelSupport\Contracts\DataManagement;
+use Hanafalah\LaravelSupport\Contracts\Supports\DataManagement;
 
 interface Disease extends DataManagement
 {
--- src/Contracts/ModuleDisease.php
+++ src/Contracts/ModuleDisease.php
@@ -5,6 +5,6 @@ namespace Hanafalah\ModuleDisease\Contracts;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
-use Hanafalah\LaravelSupport\Contracts\DataManagement;
+use Hanafalah\LaravelSupport\Contracts\Supports\DataManagement;
 
 interface ModuleDisease extends DataManagement {}
