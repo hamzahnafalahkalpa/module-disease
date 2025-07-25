62bf1fb0068afaf008695bfb1caeac84a42a49fa
Hamzah <hamzahnafalah@gmail.com>
2025-07-26 00:51:34 +0700

    update modules

--- src/Routes/api/illness.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Hanafalah\ModuleDisease\Controllers\API\Illness\IllnessController;
-use Illuminate\Support\Facades\Route;
-
-Route::apiResource('illness', IllnessController::class)
-    ->parameters(['illness' => 'id']);
